f951563b78e10904b825f11f76d02238ed7bba51
kapibarakapi565 <kapibarakapi565@gmail.com>
2025-04-01 20:30:34 +0900

    複数タスクの追加機能と一覧表示

--- main.rb
+++ main.rb
@@ -1,8 +1,15 @@
 tasks = []
 
-puts "タスクを入力してください"
+puts "タスクを入力してください。\n終了したいときは「end」を入力してください。"
 
-input = gets.chomp
-tasks.push(input)
+loop do
+  input = gets.chomp
+  if input == "end"
+    break
+  end
+  tasks.push(input)
+end
 
-p tasks
+tasks.each_with_index do |task, i|
+  puts "#{i + 1}: #{task}"
+end
