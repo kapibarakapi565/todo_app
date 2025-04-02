5bb78ff3fbe00c3cf9edd8f8021a9545bc3d50fb
kapibarakapi565 <kapibarakapi565@gmail.com>
2025-04-02 20:27:12 +0900

    削除機能とセクション分け

--- main.rb
+++ main.rb
@@ -1,5 +1,5 @@
 tasks = []
-
+# ===タスクの登録===
 puts "タスクを入力してください。\n終了したいときは「end」を入力してください。"
 
 loop do
@@ -10,6 +10,29 @@ loop do
   tasks.push(input)
 end
 
+# ===タスクの一覧表示===
 tasks.each_with_index do |task, i|
   puts "#{i + 1}: #{task}"
 end
+
+# ===タスクの削除===
+if tasks.empty?
+  puts "削除するタスクはありません"
+else
+  puts "削除したいタスクの番号を選択してください。\n0を入力すると削除を終了します。"
+  loop do
+    input = gets.to_i
+    if input < 0 || input > tasks.length
+      puts "無効な番号です。"
+    elsif input == 0
+      break
+    else
+      tasks.delete_at(input - 1)
+      tasks.each_with_index do |task, i|
+        puts "#{i + 1}: #{task}"
+      end
+    end
+  end
+end
+
+
